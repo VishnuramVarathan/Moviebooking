ef6f0c95224247d6ca1dd3dbae5e4e3fb6f2c99e
VishnuramVarathan <vishnu22997@gmail.com>
2022-05-28 10:58:38 +0530

    Initialcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './admin/user/user.component';
+import { UserRolesComponent } from './admin/user-roles/user-roles.component';
+import { UserGroupsComponent } from './admin/user-groups/user-groups.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    UserComponent,
+    UserRolesComponent,
+    UserGroupsComponent
   ],
   imports: [
     BrowserModule,
